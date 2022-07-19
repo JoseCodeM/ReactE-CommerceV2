2fd59d489b0019f8f18afcc1c9eca6b1d82cbe4c
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-07-18 20:24:48 -0400

    deleted useless files and added basic nav bar

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    </>
+  )
+}
