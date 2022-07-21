520eaf855c952c2c2ea5cd8e88f70283d8061a7f
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-07-21 09:08:35 -0400

    added routes and navbar links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import './App.css';
-import  NavBar from "./components/NavBar"
+import './Styling/App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Pages/Home';
+import Contact from './Pages/Contact';
+import Products from './Pages/Products';
+// import  NavBar from "./components/NavBar"
+import Error from './Pages/Error';
 
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-    </div>
-  );
+  <Router>
+    <nav>
+            <ul className='NavContainer'>
+                <Link to="/">Home</Link>
+                <Link to="/products">Products</Link>
+                <Link to="/contact">Contact</Link>
+            </ul>
+        </nav>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+      <Route path="/products" element={<Products />}/>
+      <Route path="/contact" element={<Contact />}/>
+      <Route path='*' element={<Error />}/>
+    </Routes>
+  </Router>
+  )
 }
 
 export default App;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,15 +1,16 @@
-import React from 'react'
+// import React from 'react'
+// import { BrowserRouter as Link } from 'react-router-dom'
 
-export default function NavBar() {
-  return (
-    <>
-        <nav>
-            <ul>
-                <li>Home</li>
-                <li>Products</li>
-                <li>Contact</li>
-            </ul>
-        </nav>
-    </>
-  )
-}
+// export default function NavBar() {
+//   return (
+//     <>
+//         <nav>
+//             <ul className='NavContainer'>
+//                 <Link to="/">Home</Link>
+//                 <Link to="/products">Products</Link>
+//                 <Link to="/contact">Contact</Link>
+//             </ul>
+//         </nav>
+//     </>
+//   )
+// }
