611011cfa8f3bcf6b2889cd671f86d754d2cdc47
Jose Eduardo Martinez <joseeduardomartinez@MacBook-Pro-3.local>
2022-07-28 18:28:36 -0400

    testing local db

--- server/index.js
+++ server/index.js
@@ -3,24 +3,14 @@ const app = express();
 const mysql = require('mysql');
 
 const db = mysql.createPool({
-    connectionLimit: 1000,
-    connectTimeout: 60 * 60 * 1000,
-    acquireTimeout: 60 * 60 * 1000,
-    timeout: 60 * 60 * 1000,
-    host: "ecommv2.cb4unrnnwl4a.us-east-1.rds.amazonaws.com",
-    port: "3001",
-    user: "admin",
+    // host: "ecommv2.cb4unrnnwl4a.us-east-1.rds.amazonaws.com",
+    host: "localhost",
+    port: "3306",
+    user: "root",
     password: "password1234",
-    database: "Ecomm"
+    database: "guidedInquiry"
 })
 
-app.get('/products', (req, res) => {
-    db.query('SELECT * FROM Ecommv2.Products', (err,result) => {
-        if (err) throw err;
-        console.log('request sent');
-        res.send(result);
-    })
-});
 
 app.listen(3001, () => {
     console.log('running on port 3001')
